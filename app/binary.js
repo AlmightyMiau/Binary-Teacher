554300ce24a5ba36adb9f6bd19cb4c2150b32bfb
AlmightyMiau <almightymeow612@gmail.com>
2024-10-31 16:01:39 -0700

    I'm gonna use vs code :/

--- app/binary.js
+++ app/binary.js
@@ -1,17 +1,28 @@
-document.body.style.background = 'purple';
+let asciiObject;
 
 async function fetchAsciiData() {
-	fetch('http://localhost:3000/ascii')
+	await fetch('http://localhost:3000/ascii')
 		.then((response) => {
 			return response.json();
 		})
 		.then((data) => {
-			document.getElementById('ascii-data').textContent = JSON.stringify(data, null, 2);
+			asciiObject = data;
+			// document.getElementById('ascii-data').textContent = JSON.stringify(data, null, 2);
 		})
 		.catch((error) => {
 			console.error('Error fetching ASCII data:', error);
 			document.getElementById('ascii-data').textContent = "Failed tp load ASCII data.";
 		});
+	return 0;
 }
 
 fetchAsciiData();
+
+function main() {
+	// document.getElementById('ascii-data').textContent = JSON.stringify(asciiObject, null, 2);
+	document.getElementById('item-binary').textContent = asciiObject[0].binary;
+	document.getElementById('item-hex').textContent = asciiObject[0].hex;
+	document.getElementById('item-char').textContent = asciiObject[0].char;
+}
+
+setTimeout(main, 50);
